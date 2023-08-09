92991f4e1570f9ab6e71db49db8f7c7332233b4a
Shenghao Xie <xshxiegudong45@gmail.com>
2023-08-09 16:18:34 +0800

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { axios } from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const LoginPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+  });
+
+  const onLogin = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Login</h1>
+      <hr />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        onClick={onLogin}
+      >
+        Login here
+      </button>
+      <Link href="/signup">Visit Signup page</Link>
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { axios } from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const SignupPage = () => {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="username"
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">Password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        id="password"
+        type="text"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+      />
+      <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:bnorder-gray-600"
+        onClick={onSignup}
+      >
+        Signup here
+      </button>
+      <Link href="/login">Visit login page</Link>
+    </div>
+  );
+};
+
+export default SignupPage;
